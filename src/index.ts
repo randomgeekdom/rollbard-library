d1275f70169c4454db2acf9217ef37a77f75a40d
randomgeekdom <randomgeekdom@gmail.com>
2022-01-27 20:49:31 -0600

    updating

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
+export * from "./Generators/GenderGenerator";
+export * from "./Generators/CityNameGenerator";
 export * from "./Generators/NameGenerator";
+export * from "./Generators/NPCGenerator";
+export * from "./Generators/TrapGenerator";
+export * from "./Services/Randomizer";
