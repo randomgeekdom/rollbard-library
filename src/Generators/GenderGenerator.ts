0a060b1f325609b7ceffe127d6c24e6a37c45d2c
randomgeekdom <randomgeekdom@gmail.com>
2022-12-11 20:21:45 -0600

    Update Gender generator to include other

--- src/Generators/GenderGenerator.ts
+++ src/Generators/GenderGenerator.ts
@@ -4,6 +4,9 @@ import Randomizer from "../Services/Randomizer";
 export default class GenderGenerator {
     constructor(private randomizer: Randomizer){}
     public GetGender(): Gender {
+        if(this.randomizer.GetRandomInt(100) < 10){
+            return Gender.Other;
+        }
         return this.randomizer.GetRandomBool() ? Gender.Female : Gender.Male;
     }
 }
