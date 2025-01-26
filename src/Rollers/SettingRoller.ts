89034277f7b50bef54bd5c6f11385445bcd8cf3f
Anthony C. Jones <randomgeekdom@gmail.com>
2025-01-26 16:52:25 -0600

    Adding castle to Setting roller

--- src/Rollers/SettingRoller.ts
+++ src/Rollers/SettingRoller.ts
@@ -2,6 +2,7 @@ import FromListRoller from "./FromListRoller";
 
 export default class SettingRoller extends FromListRoller<string> {
     protected List = [
+        "Castle",
         "Fort",
         "Mansion",
         "School",
